038c23932cd52a063288005b62bd9ebcf288138f
Khas195 <tungthanhcao1091995@gmail.com>
2019-08-03 15:21:32 +0200

    add smooth aceleration

--- Assets/Scripts/Movement/Movement2D.cs
+++ Assets/Scripts/Movement/Movement2D.cs
@@ -2,46 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement2D : IMovement 
+public class Movement2D : IMovement
 {
     [SerializeField]
     Rigidbody2D body2D = null;
-    [SerializeField]
-    float timeUntilMaxSpeed = 0.2f;
     float currentSpeed;
     bool inMotion = false;
-    float acceleration = 0;
+    [SerializeField]
+    float smoothAcceleration = 0;
     // Start is called before the first frame update
     void Start()
     {
-        if (this.data == null) {
+        if (this.data == null)
+        {
             Definition.InitalizeErrors("Move Data is null in " + this);
         }
-        
+
     }
     void Update()
     {
-        if (inMotion == true && currentSpeed < data.runSpeed) {
-            currentSpeed += acceleration * Time.deltaTime;
-        } else if (inMotion == false && currentSpeed > 0)
+        if (inMotion == true && currentSpeed < data.runSpeed)
         {
-            currentSpeed -= acceleration * Time.deltaTime;
+            currentSpeed = Mathf.Lerp(currentSpeed, data.runSpeed, smoothAcceleration);
         }
     }
     public override void Move(float forward, float side)
     {
-        acceleration = data.runSpeed / timeUntilMaxSpeed;
-        if (forward != 0 || side != 0) {
+        if (forward != 0 || side != 0)
+        {
             inMotion = true;
-        } else {
+        }
+        else
+        {
             inMotion = false;
+            currentSpeed = 0;
         }
         var vel = new Vector3();
         vel.y = forward * currentSpeed * Time.deltaTime;
         vel.x = side * currentSpeed * Time.deltaTime;
-        body2D.MovePosition(body2D.transform.position + vel );
+        body2D.MovePosition(body2D.transform.position + vel);
     }
-    public float GetCurrentSpeed() {
+    public float GetCurrentSpeed()
+    {
         return currentSpeed;
     }
     public override void SignalJump()
