022922d8fe11b99c7d55f890305a0fefb3ee77aa
Khas195 <tungthanhcao1091995@gmail.com>
2019-08-08 14:06:34 +0200

    Add sorting manager Readjust some colliders

---
+++ Assets/Scripts/Core/LayerSorting/FrontBackSorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontBackSorting : MonoBehaviour
+{
+    [SerializeField]
+    Transform host;
+    [SerializeField]
+    bool useSelfAsHost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (useSelfAsHost) {
+            host = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public Vector3 GetHostPosition()
+    {
+        return host.position;
+    }
+
+    public void SetHostPosition(Vector3 pos)
+    {
+        host.transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Core/LayerSorting/FrontBackSortingManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontBackSortingManager : MonoBehaviour
+{
+    [SerializeField]
+    List<FrontBackSorting> sortingObjects;
+    [SerializeField]
+    Transform character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sortingList = GameObject.FindObjectsOfType<FrontBackSorting>();
+        foreach (var sortingTarget in sortingList)
+        {
+            sortingObjects.Add(sortingTarget);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var target in sortingObjects)
+        {
+            var pos = target.GetHostPosition();
+            if (character.position.y > pos.y)
+            {
+                pos.z = character.position.z - 1;
+            }
+            else if (character.position.y < pos.y)
+            {
+                pos.z = character.position.z + 1;
+            }
+            else
+            {
+                pos.z = character.position.z;
+            }
+            target.SetHostPosition(pos);
+        }
+    }
+}
